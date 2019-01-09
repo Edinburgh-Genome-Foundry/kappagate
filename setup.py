5b55c06ee33804903289a98fb957e8a6070ad33e
Zulko <valentin.zulkower@gmail.com>
2019-01-09 21:28:58 +0000

    added topkappy dependency

--- setup.py
+++ setup.py
@@ -15,5 +15,5 @@ setup(
     license='MIT',
     keywords="DNA assembly synthetic biology golden gate",
     packages=find_packages(exclude='docs'),
-    install_requires=['kappagate', 'networkx', 'tatapov', 'matplotlib',
+    install_requires=['topkappy', 'networkx', 'tatapov', 'matplotlib',
                       'dnacauldron'])
